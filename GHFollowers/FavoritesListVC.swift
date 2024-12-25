a878d9372fd1cc3db54c04036a43c8fa6e905138
peyman7585 <abbaszadeh.8575@gmail.com>
2024-12-25 17:03:33 +0330

    make Controllers

--- GHFollowers/FavoritesListVC.swift
+++ GHFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GHFollowers
 //
 //  Created by Peyman on 12/25/24.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
-    }
 
+        view.backgroundColor = .systemGreen
+    }
+    
+
+ 
 
 }
-
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Peyman on 12/25/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+}
