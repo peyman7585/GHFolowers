0623a8f0ff3f83ea90a372faadf5295e63a99871
peyman7585 <abbaszadeh.8575@gmail.com>
2024-12-25 15:17:17 +0330

    start projet

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Peyman on 12/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
