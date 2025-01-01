0ad4fc5dc21137ab5e60ff2d2a84b302b64af3c8
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-01-01 15:15:12 +0330

    make folders and models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Peyman on 1/1/25.
+//
+
+import Foundation
+
+struct Follower: Codable{
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHFollowers/Model/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Peyman on 1/1/25.
+//
+
+import Foundation
+
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var Name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
