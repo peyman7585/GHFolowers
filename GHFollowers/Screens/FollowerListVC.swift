1c08367fd0ad8e95c35bd6d73b23f7a3db4f9546
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-01-20 14:51:23 +0330

    add loading view

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -48,9 +48,10 @@ class FollowerListVC: UIViewController {
         
     }
     func getFollowers(username : String ,page : Int) {
-        
+        showLoadingView()
            NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
                guard let self = self else { return }
+               self.dismissLoadingView()
                switch result {
                case .success(let followers):
                    if followers.count > 100 {  self.hasMoreFollower = false }
@@ -100,7 +101,6 @@ extension FollowerListVC: UICollectionViewDelegate {
             page += 1
             getFollowers(username: username, page: page)
         }
-        
         
     }
 }
