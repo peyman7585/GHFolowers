80f1bd38206038d25096446664bd202b254b9a96
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-01-21 14:15:36 +0330

    add search bar

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -13,6 +13,7 @@ class FollowerListVC: UIViewController {
      
     var username: String!
     var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollower = true
     var collocationView: UICollectionView!
@@ -22,6 +23,7 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureCollectionView()
         getFollowers(username: username, page: page)
         configureDataSource()
@@ -47,6 +49,16 @@ class FollowerListVC: UIViewController {
         collocationView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
         
     }
+    
+    func configureSearchController(){
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Search for a username"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        
+    }
     func getFollowers(username : String ,page : Int) {
         showLoadingView()
            NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
@@ -62,7 +74,7 @@ class FollowerListVC: UIViewController {
                        DispatchQueue.main.sync { self.showEmptyStateView(with: message, in: self.view) }
                        return
                    }
-                   self.updateData()
+                   self.updateData(on: self.followers)
                    
                case .failure(let error):
                    self.presentGFAlertMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
@@ -81,7 +93,7 @@ class FollowerListVC: UIViewController {
     }
     
     
-    func updateData(){
+    func updateData(on followers: [Follower]){
         var snapshot = NSDiffableDataSourceSnapshot<Section , Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -109,4 +121,18 @@ extension FollowerListVC: UICollectionViewDelegate {
         }
         
     }
+}
+
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
+        updateData(on: filteredFollowers)
+        
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
+    }
+    
 }
