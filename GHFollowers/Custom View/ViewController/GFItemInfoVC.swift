f4cb784c27b053edebf27924270004ea384f3305
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-01-25 15:48:35 +0330

    add stackView

---
+++ GHFollowers/Custom View/ViewController/GFItemInfoVC.swift
@@ -0,0 +1,58 @@
+//
+//  GFItemInfoVC.swift
+//  GHFollowers
+//
+//  Created by Peyman on 1/25/25.
+//
+
+import UIKit
+
+class GFItemInfoVC: UIViewController {
+
+    let stackView = UIStackView()
+    let itemInfoViewOne = GFItemInfoView()
+    let itemInfoViewTwo = GFItemInfoView()
+    let actionButton = UIButton()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackGroundView()
+        layoutUI()
+        configureStackView()
+    }
+    
+    
+    private func configureBackGroundView(){
+        view.layer.cornerRadius = 18
+        view.backgroundColor = .secondarySystemBackground
+    }
+    
+    private func configureStackView(){
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoViewOne)
+        stackView.addArrangedSubview(itemInfoViewTwo)
+    }
+ 
+    private func layoutUI(){
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor,constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+            
+        ])
+    }
+}
---
+++ GHFollowers/Custom View/ViewController/GFRepoItemVC.swift
@@ -0,0 +1,12 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Peyman on 1/25/25.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+}
